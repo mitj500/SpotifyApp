52e57c2d91c3bb6ea62fee8136f6afa36a94fa11
mitj500 <mitjain9987900@gmail.com>
2024-06-12 12:38:08 +0530

    second update

--- Ly/src/components/Error.jsx
+++ Ly/src/components/Error.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-const Error = () => (
-  <div>Error</div>
-);
+function Error() {
+  return (
+    <div className="w-full flex justify-center items-center">
+      something went wrong.Please try again.
+      </div>
+  )
+}
 
-export default Error;
+export default Error
--- Ly/src/components/SongCard.jsx
+++ Ly/src/components/SongCard.jsx
@@ -1,5 +1,14 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import React from 'react'
+import {link} from 'react-router-dom'
+import { useDispatch } from 'react-redux' 
+import PlayPause from './PlayPause'
+import { playPause,setActiveSong } from '../redux/features/playerSlice'
+function SongCard({song,i}) {
+  return (
+    <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slieup roudned-lg cursor-pointer'> 
+      <div className=''/>
+      </div>
+  )
+}
 
-export default SongCard;
+export default SongCard
