18d60deee8b22223c175c95e8b0e10b5536e0c9f
mitj500 <mitjain9987900@gmail.com>
2024-06-12 18:34:42 +0530

    second

--- Ly/src/components/PlayPause.jsx
+++ Ly/src/components/PlayPause.jsx
@@ -1,5 +1,17 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import React from 'react'
+import {FaPauseCircle,FaPlayCircle}from 'react-icons'
 
-export default PlayPause;
+
+const PlayPause({isPlaying,activeSong,song,handelPause,handelPlay})=> (isPlaying&& activeSong?.title==song.title?(
+    <FaPauseCircle
+      size={35}
+      className="text-gray-300" 
+      onClick={handelPause}     
+    />):
+    (<FaPlayCircle
+      size={35}
+      className="text-gray-300"
+      onClick={handelPlay}    
+    />))
+
+export default PlayPause
--- Ly/src/components/SongCard.jsx
+++ Ly/src/components/SongCard.jsx
@@ -3,11 +3,31 @@ import {link} from 'react-router-dom'
 import { useDispatch } from 'react-redux' 
 import PlayPause from './PlayPause'
 import { playPause,setActiveSong } from '../redux/features/playerSlice'
-function SongCard({song,i}) {
+function SongCard({song,isPlaying,activeSong,i,data}) {
+
+  const handlepauseClick=()=>{ };
+    const handleplayClick=()=>{ };
   return (
+    
     <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slieup roudned-lg cursor-pointer'> 
-      <div className=''/>
+      <div>
+        <PlayPause 
+        isPlaying={isPlaying} 
+        activeSong={activeSong}
+        song= {song}
+        handlepause={handlepause}
+        handleplay ={handleplay}  />
       </div>
+      <p className='flex flex-col mt-4 font-semibold truncate'>
+        <link to ={`/songs/${song?.key}`}>
+        {song.title}
+        </link>
+      </p>
+      <p>
+        <link to ={song.artists ?`/artists/${song?.artisits[0]?.adamid}`:`/top-artists`}></link>
+      </p>
+      </div>
+
   )
 }
 
